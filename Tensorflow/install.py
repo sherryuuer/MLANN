245dd497e660b4fd47f589612026ca424276e991
sallyw <sallyw@sallywnoMacBook-Air.local>
2023-12-22 12:14:27 +0900

    update

--- Tensorflow/install.py
+++ Tensorflow/install.py
@@ -4,3 +4,16 @@
 # >> hello = tf.constant('Hello, Tensorflow!')
 # >> sess = tf.Session()
 # >> print(sess.run(hello))
+
+
+python3 -m venv --system-site-packages ./venv
+source ./venv/bin/activate  # sh, bash, or zsh
+
+pip install --upgrade pip
+
+pip list  # show packages installed within the virtual environment
+deactivate  # don't exit until you're done using TensorFlow
+
+pip install --upgrade tensorflow
+
+https://www.tensorflow.org/install/pip?hl=zh-cn#ubuntu-macos
